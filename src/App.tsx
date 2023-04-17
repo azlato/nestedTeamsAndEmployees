c83e3ba5ae24172714dd9da114f51756eb7f2790
Adam Zlatohlavek <zlatohlavecek@gmail.com>
2023-04-17 17:49:17 +0200

    installed Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
+const lightTheme = createTheme({ palette: { mode: 'light' } });
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <ThemeProvider
+      theme={lightTheme}
+    >
+      <CssBaseline />
+      <Container maxWidth="xl" sx={{ p: 3 }}>
+        Content
+      </Container>
+    </ThemeProvider>
   );
 }
 
