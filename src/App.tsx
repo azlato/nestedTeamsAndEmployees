eb278cf87c50f048d19be23605166072ba2286b4
Adam Zlatohlavek <zlatohlavecek@gmail.com>
2023-04-17 17:54:50 +0200

    set primary and secondary color

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
-const lightTheme = createTheme({ palette: { mode: 'light' } });
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#02D076',
+    },
+    secondary: {
+      main: '#10BFFC',
+    },
+  },
+});
 
 function App() {
   return (
-    <ThemeProvider
-      theme={lightTheme}
-    >
+    <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <Container maxWidth="xl" sx={{ p: 3 }}>
         Content
