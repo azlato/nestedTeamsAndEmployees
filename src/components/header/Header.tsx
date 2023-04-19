5249216bd493fd15c2ef053596b9c13dd5392a83
Adam Zlatohlavek <zlatohlavecek@gmail.com>
2023-04-19 10:34:02 +0200

    updated header logo style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,6 @@ import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import alvenoLogo from '../../assets/alveno-logo-dark.svg';
 
 function Header() {
@@ -12,20 +11,17 @@ function Header() {
       <AppBar>
         <Container maxWidth="xl">
           <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
+            <Box
               component="a"
               href="/"
               sx={{
+                display: 'flex',
                 mr: 2,
-                fontWeight: 700,
-                color: 'inherit',
-                textDecoration: 'none',
+                width: [60, 120],
               }}
             >
-              <Box component="img" src={alvenoLogo} alt="" sx={{ width: [60, 120] }} />
-            </Typography>
+              <img src={alvenoLogo} alt="Alveno by Iresoft logo" />
+            </Box>
           </Toolbar>
         </Container>
       </AppBar>
